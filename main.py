ffa87f9b6fe8071a69da94445e75e9bb9123c757
Ruddy3702 <rudraag3@gamil.com>
2025-12-12 16:44:17 +0530

    error tha

--- main.py
+++ main.py
@@ -252,6 +252,5 @@ def transactions():
 def portfolio():
     return render_template("portfolio.html", logged_in= current_user.is_authenticated)
 
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
